bd5cb1b3f3dc429e52025c67554b3ace1d001cd6
James Wilson <me@unbui.lt>
2018-12-17 09:36:40 +0000

    add proper tests and do part 2 (answer not right though)

--- day15/solution/day15.rs
+++ day15/solution/day15.rs
@@ -1,39 +1,42 @@
-use self::battle::{State,Opts,UnitType::*};
+use self::battle::{State,Opts};
 
 fn main() {
-
     let filename = std::env::args().nth(1).expect("need puzzle input");
     let input: String = std::fs::read_to_string(filename).expect("can't open file");
 
-    // This is a tricky puzzle, so run all of the provided/found samples on each try
-    // to make sure that any changes I make doesn't break anything obvious:
-    run_tests();
-
     // Star 1: How many rounds would things last with damage of 3:
-    let (round, total_health) = run(&input, Opts::elf_damage(3));
+    let (round, total_health) = run_part1(&input);
     println!("Star 1: {} ({} rounds, {} health)",  total_health * round, round, total_health);
 
-    // Star 2: How much damage do elves have to be able to do to win without a single loss?
-    let mut outcome = 0;
-    'outer: for damage in 4.. {
-        let mut state = State::from_str(&input, Opts::elf_damage(damage));
-        let mut round = 0;
-        let elves_count = state.units().filter(|u| u.ty == Elf).count();
-        while state.round() {
-            round += 1;
-            if elves_count != state.units().filter(|u| u.ty == Elf).count() {
-                continue 'outer;
-            }
-        }
-        let total_health: i32 = state.units().map(|u| u.health).sum();
-        outcome = round * total_health;
-        break;
-    }
-    println!("Star 2: {}", outcome);
+    // Star 2: Keep upping elf damage until they win without a loss. What's the outcome?
+    let (round, total_health, damage) = run_part2(&input);
+    println!("Star 2: {} ({} rounds, {} health, damage {})",  total_health * round, round, total_health, damage);
 }
 
-fn run(s: &str, opts: Opts) -> (i32,i32) {
+// Star 1:
+fn run_part1(s: &str) -> (i32, i32) {
+    let opts = Opts::new().elf_damage(3);
     let mut state = State::from_str(&s, opts);
+    run(&mut state)
+}
+
+// Star 2:
+fn run_part2(s: &str) -> (i32, i32, i32) {
+    let mut damage = 4;
+    loop {
+        let opts = Opts::new().elf_damage(damage).stop_on_elf_death(true);
+        let mut state = State::from_str(&s, opts);
+        let (round, total_health) = run(&mut state);
+        if state.is_battle_over() && !state.did_an_elf_die() {
+            return (round, total_health, damage);
+        } else {
+            damage += 1;
+        }
+    }
+}
+
+// Run a given state to completion and return the outcome:
+fn run(state: &mut State) -> (i32,i32) {
     let mut round = 0;
     while state.round() {
         round += 1;
@@ -42,6 +45,9 @@ fn run(s: &str, opts: Opts) -> (i32,i32) {
     (round, total_health)
 }
 
+// ##################################################################
+// # This module contains the things we need to simulate the battle #
+// ##################################################################
 mod battle {
     use std::collections::{ HashMap, HashSet };
     use std::fmt::{self,Display};
@@ -54,11 +60,22 @@ mod battle {
     #[derive(Copy,Clone,Debug)]
     pub struct Opts {
         elf_damage: i32,
+        stop_on_elf_death: bool
     }
-
     impl Opts {
-        pub fn elf_damage(d: i32) -> Opts {
-            Opts { elf_damage: d }
+        pub fn new() -> Opts {
+            Opts {
+                elf_damage: 3,
+                stop_on_elf_death: false
+            }
+        }
+        pub fn elf_damage(mut self, d: i32) -> Opts {
+            self.elf_damage = d;
+            self
+        }
+        pub fn stop_on_elf_death(mut self, b: bool) -> Opts {
+            self.stop_on_elf_death = b;
+            self
         }
     }
 
@@ -66,13 +83,18 @@ mod battle {
     //
     pub struct State {
         units: HashMap<Coords, Unit>,
-        walls: HashSet<Coords>
+        walls: HashSet<Coords>,
+        stop_on_elf_death: bool,
+        starting_elves: i32,
+        elves_remaining: i32,
+        goblins_remaining: i32
     }
-
     impl State {
         pub fn from_str(s: &str, opts: Opts) -> State {
             let mut units = HashMap::new();
             let mut walls = HashSet::new();
+            let mut starting_elves = 0;
+            let mut starting_goblins = 0;
             for (y, line) in s.trim().lines().enumerate() {
                 for (x, byte) in line.trim().bytes().enumerate() {
                     let p = Coords{ x:x as i32, y:y as i32 };
@@ -84,6 +106,7 @@ mod battle {
                                 damage:opts.elf_damage,
                                 health:STARTING_HEALTH
                             });
+                            starting_elves += 1;
                         },
                         b'G' => {
                             units.insert(p, Unit{
@@ -91,19 +114,29 @@ mod battle {
                                 damage:3,
                                 health:STARTING_HEALTH
                             });
+                            starting_goblins += 1;
                         },
                          _  => { /* ignore other bits */ }
                     }
                 }
             }
-            State { units, walls }
+            State {
+                units,
+                walls,
+                starting_elves,
+                elves_remaining: starting_elves,
+                goblins_remaining: starting_goblins,
+                stop_on_elf_death: opts.stop_on_elf_death
+            }
         }
         pub fn round(&mut self) -> bool {
 
             // Sort units by reading order so that we know how to progress:
             let mut unit_coords: Vec<Coords> = self.units.keys().cloned().collect();
-            let mut dead_units = HashSet::new();
             unit_coords.sort();
+
+            // Track dead units so we don't try to use those coords:
+            let mut dead_units = HashSet::new();
 
             // Bail out if there's going to be nothing to do:
             if self.is_finished() {
@@ -144,6 +177,10 @@ mod battle {
                     let enemy = self.units.get_mut(&enemy_coords).unwrap();
                     enemy.health -= unit.damage;
                     if enemy.health <= 0 {
+                        match enemy.ty {
+                            Elf => { self.elves_remaining -= 1; },
+                            Goblin => { self.goblins_remaining -= 1; }
+                        }
                         self.units.remove(&enemy_coords);
                         dead_units.insert(enemy_coords);
                         finished_early = self.is_finished();
@@ -156,16 +193,13 @@ mod battle {
             self.units.values().cloned()
         }
         pub fn is_finished(&self) -> bool {
-            let mut elves = 0;
-            let mut goblins = 0;
-            for unit in self.units.values() {
-                if unit.ty == Elf {
-                    elves += 1;
-                } else {
-                    goblins += 1;
-                }
-            }
-            elves == 0 || goblins == 0
+            (self.stop_on_elf_death && self.did_an_elf_die()) || self.is_battle_over()
+        }
+        pub fn is_battle_over(&self) -> bool {
+            self.elves_remaining == 0 || self.goblins_remaining == 0
+        }
+        pub fn did_an_elf_die(&self) -> bool {
+            self.elves_remaining < self.starting_elves
         }
         fn adjacent_units(&self, coords: Coords, ty: UnitType) -> impl Iterator<Item=(Coords,Unit)> + '_ {
             coords.adjacent()
@@ -181,6 +215,12 @@ mod battle {
                 // when attacking, find unit with lowest health first, reading order if tie:
                 .min_by_key(|(c,unit)| (unit.health, *c))
                 .map(|(c,_)| c)
+        }
+        fn next_available_coords<'a>(&'a self, coords: Coords) -> impl Iterator<Item=Coords> + 'a {
+            coords.adjacent()
+                .into_iter()
+                .filter(move |c| !self.walls.contains(c))
+                .filter(move |c| !self.units.contains_key(c))
         }
         fn step_to_nearest_unit(&self, start_coords: Coords, ty: UnitType) -> Option<Coords> {
 
@@ -239,12 +279,6 @@ mod battle {
                 path.last().map(|&c| c) // use the one just after.
             };
             next_coords
-        }
-        fn next_available_coords<'a>(&'a self, coords: Coords) -> impl Iterator<Item=Coords> + 'a {
-            coords.adjacent()
-                .into_iter()
-                .filter(move |c| !self.walls.contains(c))
-                .filter(move |c| !self.units.contains_key(c))
         }
     }
 
@@ -316,76 +350,197 @@ mod battle {
 
 }
 
-fn run_tests() {
+// ###############################################
+// # A bunch of tests to help ensure correctness #
+// ###############################################
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    let opts = Opts::elf_damage(3);
+    #[test]
+    fn part1() {
+        assert_eq!(run_part1(r"
+            #######
+            #.G...#
+            #...EG#
+            #.#.#G#
+            #..G#E#
+            #.....#
+            #######
+        "), (47, 590));
 
-    assert_eq!(run(r"
-        ####
-        ##E#
-        #GG#
-        ####
-    ", opts), (67, 200));
+        assert_eq!(run_part1(r"
+            #######
+            #G..#E#
+            #E#E.E#
+            #G.##.#
+            #...#E#
+            #...E.#
+            #######
+        "), (37, 982));
 
-    assert_eq!(run(r"
-        #######
-        #.G...#
-        #...EG#
-        #.#.#G#
-        #..G#E#
-        #.....#
-        #######
-    ", opts), (47, 590));
+        assert_eq!(run_part1(r"
+            #######
+            #E..EG#
+            #.#G.E#
+            #E.##E#
+            #G..#.#
+            #..E#.#
+            #######
+        "), (46, 859));
 
-    assert_eq!(run(r"
-        #######
-        #G..#E#
-        #E#E.E#
-        #G.##.#
-        #...#E#
-        #...E.#
-        #######
-    ", opts), (37, 982));
+        assert_eq!(run_part1(r"
+            #######
+            #E.G#.#
+            #.#G..#
+            #G.#.G#
+            #G..#.#
+            #...E.#
+            #######
+        "), (35, 793));
 
-    assert_eq!(run(r"
-        #######
-        #E..EG#
-        #.#G.E#
-        #E.##E#
-        #G..#.#
-        #..E#.#
-        #######
-    ", opts), (46, 859));
+        assert_eq!(run_part1(r"
+            #######
+            #.E...#
+            #.#..G#
+            #.###.#
+            #E#G#G#
+            #...#G#
+            #######
+        "), (54, 536));
 
-    assert_eq!(run(r"
-        #######
-        #E.G#.#
-        #.#G..#
-        #G.#.G#
-        #G..#.#
-        #...E.#
-        #######
-    ", opts), (35, 793));
+        assert_eq!(run_part1(r"
+            #########
+            #G......#
+            #.E.#...#
+            #..##..G#
+            #...##..#
+            #...#...#
+            #.G...G.#
+            #.....G.#
+            #########
+        "), (20, 937));
+    }
 
-    assert_eq!(run(r"
-        #######
-        #.E...#
-        #.#..G#
-        #.###.#
-        #E#G#G#
-        #...#G#
-        #######
-    ", opts), (54, 536));
+    #[test]
+    fn part1_extra() {
+        assert_eq!(run_part1(r"
+            ####
+            ##E#
+            #GG#
+            ####
+        "), (67, 200));
 
-    assert_eq!(run(r"
-        #########
-        #G......#
-        #.E.#...#
-        #..##..G#
-        #...##..#
-        #...#...#
-        #.G...G.#
-        #.....G.#
-        #########
-    ", opts), (20, 937));
+        assert_eq!(run_part1(r"
+            #####
+            #GG##
+            #.###
+            #..E#
+            #.#G#
+            #.E##
+            #####
+        "), (71, 197));
+
+        assert_eq!(run_part1(r"
+            ################
+            #.......G......#
+            #G.............#
+            #..............#
+            #....###########
+            #....###########
+            #.......EG.....#
+            ################
+        "), (38, 486));
+
+        assert_eq!(run_part1(r"
+            ################################
+            #########...####################
+            #########...###########.########
+            #########G..##########....######
+            ##########..###########...######
+            #########G...##########...######
+            #########..G.###########..######
+            ########...#.##########..#######
+            #######G#..###E######....#######
+            #######G.....#.######....#######
+            ######...G......##E......#######
+            ####...##.#..G..G.........######
+            ###..........G#####.......####.#
+            ####........G#######...........#
+            ####..G.....#########......#...#
+            ###.........#########........###
+            ##.....G.G..#########......#####
+            #...G.......#########.........##
+            #.G.........#########.E.##...###
+            ##.....G.....#######....G#.E...#
+            ##............#####...E.......##
+            #.G...........E.......#E...##.##
+            #....G........###########.....##
+            #......##...#.##################
+            #.#.........E..##.##############
+            #.#.......G.......##############
+            #.###........E....##############
+            #.####.....###....##############
+            #.#####......E..################
+            #######..........###############
+            #########..####.################
+            ################################
+        "), (149,2326));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(run_part2(r"
+            #######
+            #.G...#
+            #...EG#
+            #.#.#G#
+            #..G#E#
+            #.....#
+            #######
+        "), (29, 172, 15));
+
+        assert_eq!(run_part2(r"
+            #######
+            #E..EG#
+            #.#G.E#
+            #E.##E#
+            #G..#.#
+            #..E#.#
+            #######
+        "), (33, 948, 4));
+
+        assert_eq!(run_part2(r"
+            #######
+            #E.G#.#
+            #.#G..#
+            #G.#.G#
+            #G..#.#
+            #...E.#
+            #######
+        "), (37, 94, 15));
+
+        assert_eq!(run_part2(r"
+            #######
+            #.E...#
+            #.#..G#
+            #.###.#
+            #E#G#G#
+            #...#G#
+            #######
+        "), (39, 166, 12));
+
+        assert_eq!(run_part2(r"
+            #########
+            #G......#
+            #.E.#...#
+            #..##..G#
+            #...##..#
+            #...#...#
+            #.G...G.#
+            #.....G.#
+            #########
+        "), (30, 38, 34));
+    }
+
 }
