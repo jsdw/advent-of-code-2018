b2ccd69d540fb9599d6f7c44f03d1b4187f1a623
James Wilson <me@unbui.lt>
2018-12-20 00:34:44 +0000

    rewrite and finish day17

--- day17/solution/day17.rs
+++ day17/solution/day17.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-use self::simulation::Simulation;
+use self::tracer::Tracer;
 use regex::Regex;
 use lazy_static::lazy_static;
 
@@ -11,41 +11,75 @@ fn main() {
         .map(parse_line)
         .collect();
 
-    let walls = draw_walls(&lines);
-    let mut sim = Simulation::new(walls, 500);
-    let top = sim.top();
-    let bottom = sim.bottom();
-    let mut last_water_count = 0;
-    let mut round = 0;
-    loop {
-        sim.step();
-        let c = sim.water_count();
-println!("Water: {}", c);
-    //if round % 1000 == 0 {
-    //    println!("Round {}", round);
-    //    println!("Round {}, water: {}\n{}", round, c, sim.stringify_region(400..700, 0..1800));
-    // }
-
-        round += 1;
-        if c == last_water_count {
-            break;
-        } else {
-            last_water_count = c;
-        }
-    }
-        println!("Round {}, water: {}\n{}", round, sim.water_count(), sim.stringify_region(400..700, 0..1800));
-
-
-    let count_in_bounds = sim
-        .has_seen_water()
+    // Trace out all of the squares filled with water:
+    let walls = note_walls(&lines);
+    let mut tracer = Tracer::new(walls, 500);
+    let bottom = tracer.bottom();
+    let top = tracer.top();
+    tracer.trace();
+    let count_in_bounds = tracer
+        .visited()
         .iter()
-        .filter(|&&(_,y)| y >= top && y <= bottom )
+        .filter(|&&(_,y)| y >= top && y <= bottom)
         .count();
     println!("Star 1: {}", count_in_bounds);
 
+    // Next, work backwards from the bottom removing any water
+    // that is not contained (ie left/right/up from bottom water):
+    let contained = remove_flowing(tracer);
+    let count_contained_in_bounds = contained
+        .iter()
+        .filter(|&&(_,y)| y >= top && y <= bottom)
+        .count();
+    println!("Star 2: {}", count_contained_in_bounds);
+
 }
 
-fn draw_walls(lines: &[Line]) -> HashSet<(usize,usize)> {
+fn remove_flowing(tracer: Tracer) -> HashSet<(usize,usize)> {
+
+    // Define some utility functions to help us remove bits:
+    fn remove_from(visited: &mut HashSet<(usize,usize)>, x:usize, mut y:usize) {
+        while visited.contains(&(x,y)) {
+            visited.remove(&(x,y));
+            remove_row(visited, x, y);
+            if y > 0 {
+                y -= 1;
+            } else {
+                return;
+            }
+        }
+    }
+    fn remove_row(visited: &mut HashSet<(usize,usize)>, x:usize, y:usize) {
+        remove_row_direction(visited, x, y, true);
+        remove_row_direction(visited, x, y, false);
+    }
+    fn remove_row_direction(visited: &mut HashSet<(usize,usize)>, x:usize, y:usize, go_right:bool) {
+        let inc = |n| if go_right { n + 1 } else { n - 1 };
+        let mut next_x = inc(x);
+        while visited.contains(&(next_x,y)) {
+            visited.remove(&(next_x, y));
+            if y > 0 && visited.contains(&(next_x, y-1)) {
+                remove_from(visited, next_x, y-1);
+            }
+            next_x = inc(next_x);
+        }
+    }
+
+    // Now, find the starting points and remove:
+    let bottom = tracer.bottom();
+    let mut visited = tracer.into_visited();
+    let sinks: Vec<(usize,usize)> = visited.iter()
+        .filter(|(_,y)| *y == bottom)
+        .cloned()
+        .collect();
+
+    for (x,y) in sinks {
+        remove_from(&mut visited, x,y);
+    }
+    visited
+}
+
+fn note_walls(lines: &[Line]) -> HashSet<(usize,usize)> {
     let mut walls = HashSet::new();
     for line in lines.iter().cloned() {
         for x in line.x {
@@ -82,46 +116,35 @@ struct Line {
     y: std::ops::RangeInclusive<usize>
 }
 
-// A simple water simulation:
-//
-mod simulation {
+mod tracer {
     use std::collections::HashSet;
 
-    pub struct Simulation {
+    pub struct Tracer {
         water_x: usize,
         top: usize,
         bottom: usize,
+        sources: HashSet<(usize,usize)>,
         walls: HashSet<(usize,usize)>,
-        stuck_water: HashSet<(usize,usize)>,
-        has_seen_water: HashSet<(usize,usize)>,
-        water: HashSet<(usize,usize)>
+        visited: HashSet<(usize,usize)>
     }
-
-    impl Simulation {
-        pub fn new(walls: HashSet<(usize,usize)>, water_x: usize) -> Simulation {
-            let top = walls.iter().map(|(x,_)| *x).min().unwrap_or(0);
+    impl Tracer {
+        pub fn new(walls: HashSet<(usize,usize)>, start_x: usize) -> Tracer {
+            let top = walls.iter().map(|(_,y)| *y).min().unwrap_or(0);
             let bottom = walls.iter().map(|(_,y)| *y).max().unwrap_or(0);
-            let water = HashSet::new();
-            let stuck_water = HashSet::new();
-            let has_seen_water = HashSet::new();
-            Simulation{ water_x, walls, top, bottom, water, stuck_water, has_seen_water }
-        }
-        pub fn step(&mut self) {
-            let mut water_coords: Vec<(usize,usize)> = self.water.iter().cloned().collect();
-            // move water from the bottom up:
-            water_coords.sort_unstable_by(|(_,y1),(_,y2)| y1.cmp(y2).reverse());
-            for (x,y) in water_coords {
-                visited.insert((x,y));
-                self.step_water(x,y);
+            Tracer {
+                top,
+                bottom,
+                water_x: start_x,
+                walls: walls,
+                sources: HashSet::new(),
+                visited: HashSet::new()
             }
-            self.water.insert((self.water_x,0));
-            self.has_seen_water.insert((self.water_x,0));
         }
-        pub fn walls(&self) -> &HashSet<(usize,usize)> {
-            &self.walls
+        pub fn visited(&self) -> &HashSet<(usize,usize)> {
+            &self.visited
         }
-        pub fn water_count(&self) -> usize {
-            self.has_seen_water.iter().count()
+        pub fn into_visited(self) -> HashSet<(usize,usize)> {
+            self.visited
         }
         pub fn top(&self) -> usize {
             self.top
@@ -129,91 +152,12 @@ mod simulation {
         pub fn bottom(&self) -> usize {
             self.bottom
         }
-        pub fn has_seen_water(&self) -> &HashSet<(usize,usize)> {
-            &self.has_seen_water
-        }
-        fn step_water(&mut self, x:usize,y:usize) {
-            // water is at bottom of grid so it falls off:
-            if y >= self.bottom {
-                self.water.remove(&(x,y));
-            }
-            // wall beneath water, so can't move:
-            else if self.walls.contains(&(x,y+1)) {
-                self.water.remove(&(x,y));
-                self.stuck_water.insert((x,y)); // water will never move again.
-            }
-            // water beneath water so try to fall beside:
-            else if self.water.contains(&(x,y+1)) || self.stuck_water.contains(&(x,y+1)) {
-                if let Some((nx,ny)) = self.find_space_beside_water(x,y+1) {
-                    self.water.remove(&(x,y));
-                    self.water.insert((nx,ny));
-                    self.has_seen_water.insert((nx,ny));
-                } else {
-                    self.water.remove(&(x,y));
-                    self.stuck_water.insert((x,y)); // water will never move again.
-                }
-            }
-            // nothing beneath water so move it down one:
-            else {
-                self.water.remove(&(x,y));
-                self.water.insert((x,y+1));
-                self.has_seen_water.insert((x,y+1));
-            }
-        }
-        fn find_space_beside_water(&self, x:usize, y:usize) -> Option<(usize,usize)> {
-            // find the next available space to the left and right, if any:
-            let mut more_left  = true;
-            let mut more_right = true;
-            let mut x_left  = x;
-            let mut x_right = x;
-            let mut found_left = None;
-            let mut found_right = None;
-            while more_left || more_right {
-                if more_left {
-                    if self.walls.contains(&(x_left, y)) {
-                        more_left = false;
-                    } else if self.water.contains(&(x_left, y)) || self.stuck_water.contains(&(x_left, y)) {
-                        if x_left == 0 { more_left = false }
-                        else { x_left -= 1 }
-                    } else {
-                        more_left = false;
-                        found_left = Some((x_left, y));
-                    }
-                }
-                if more_right {
-                    if self.walls.contains(&(x_right, y)) {
-                        more_right = false;
-                    } else if self.water.contains(&(x_right, y)) || self.stuck_water.contains(&(x_right, y)) {
-                        x_right += 1;
-                    } else {
-                        more_right = false;
-                        found_right = Some((x_right, y));
-                    }
-                }
-            }
-
-            // pick one of these spaces, prioritising one with something underneath it.
-            // we add a bit of randomness to ensure all routes are eventually travelled.
-            match (found_left, found_right) {
-                (None, Some(_)) => found_right,
-                (Some(_), None) => found_left,
-                (None, None) => None,
-                (Some(_), Some(_)) => {
-                    if rand::random() {
-                        found_left
-                    } else {
-                        found_right
-                    }
-                }
-            }
-        }
+        #[allow(dead_code)]
         pub fn stringify_region(&self, xs: impl Iterator<Item=usize> + Clone, ys: impl Iterator<Item=usize>) -> String {
             let mut out = String::new();
             for y in ys {
                 for x in xs.clone() {
-                    let c = if self.water.contains(&(x,y)) {
-                        '|'
-                    } else if self.stuck_water.contains(&(x,y)) {
+                    let c = if self.visited.contains(&(x,y)) {
                         '~'
                     } else if self.walls.contains(&(x,y)) {
                         '#'
@@ -225,6 +169,68 @@ mod simulation {
                 out.push('\n');
             }
             out
+        }
+        pub fn trace(&mut self) {
+            self.trace_source(self.water_x, 0);
+        }
+        fn trace_source(&mut self, x: usize, mut y: usize) {
+            if self.sources.contains(&(x,y)) {
+                return;
+            }
+            self.sources.insert((x,y));
+
+            // trace down until we hit a wall:
+            while !self.walls.contains(&(x,y+1)) {
+                y += 1;
+                if y > self.bottom {
+                    // we have gone off the bottom; done!
+                    return;
+                }
+                self.visited.insert((x,y));
+            }
+            // We hit a wall; now trace sideways up from last
+            // (x,y) seen to fill in the container, until we
+            // overflow it (ie trace over a wall).
+            self.fill_container_from(x, y);
+        }
+        fn fill_container_from(&mut self, x: usize, mut y: usize) {
+            loop {
+                let right_done = self.fill_direction_from(x, y, true);
+                let left_done = self.fill_direction_from(x, y, false);
+                if right_done || left_done {
+                    return;
+                }
+                y -= 1;
+            }
+        }
+        fn fill_direction_from(&mut self, x: usize, y: usize, go_right: bool) -> bool {
+            let mut next_x = x;
+            let mut is_last_source_below = false;
+            while !self.walls.contains(&(next_x,y)) {
+
+                let is_source = self.sources.contains(&(next_x,y));
+                let is_wall_below = self.walls.contains(&(next_x,y+1));
+                let is_visited_below = self.visited.contains(&(next_x,y+1));
+
+                // If there's nothing below us and we just stepped over a source,
+                // stop immediately; another source would cover it.
+                if !is_visited_below && !is_wall_below && is_last_source_below {
+                    return true;
+                }
+
+                self.visited.insert((next_x, y));
+
+                // if we are above nothing, we've filled the container
+                // and our current location should be a new source to trace down:
+                if !is_wall_below && !is_visited_below {
+                    self.trace_source(next_x, y);
+                    return true;
+                }
+
+                next_x = if go_right { next_x + 1 } else { next_x - 1 };
+                is_last_source_below = is_source;
+            }
+            false
         }
     }
 }
